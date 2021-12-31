7e83fdf4d7a48c2ac2cee14ae4db6a1c649e9f1d
jafarjtown <57459852+jafarjtown@users.noreply.github.com>
2022-01-01 00:34:13 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ posts }) {
 export const getServerSideProps = async (context) => {
   let res = await axios.get('http://localhost:8000/api/post/posts/', {
     headers: {
-      'Authorization': 'Token 7cecbddec612c49cf423a6126d21bb95a5f9b486'
+      'Authorization': 'Token 832cb1cd014c37a04327a0b12ee29086e930df14'
     }
   })
   let posts = await res.data
